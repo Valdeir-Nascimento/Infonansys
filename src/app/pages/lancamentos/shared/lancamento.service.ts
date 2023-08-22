edee1afd3d697d1218f5ff6f17e0a3ddf215b3d3
Valdeir Nascimento <valdeircn11@gmail.com>
2023-08-21 21:07:29 -0300

    Implementação de metódo para listar todos os lancamentos

---
+++ src/app/pages/lancamentos/shared/lancamento.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CategoriaService } from '../../categorias/shared/categoria.service';
+import { Lancamento } from './lancamento.model';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LancamentoService {
+
+    private apiPath: string = "http://localhost:3000/lancamentos";
+
+    constructor(
+        private http: HttpClient,
+        private categoriaService: CategoriaService
+    ) { }
+
+    listar(): Observable<Lancamento[]> {
+        return this.http.get(this.apiPath).pipe(
+            catchError(this.handleError),
+            map(this.jsonDataToLancamentos)
+        );
+    }
+
+    private jsonDataToLancamentos(jsonData: any[]): Lancamento[] {
+        const entries: Lancamento[] = [];
+        jsonData.forEach(element => {
+            const entry = Object.assign(new Lancamento, element);
+            entries.push(entry);
+        });
+        return entries;
+    }
+
+    private jsonDataToLancamento(jsonData: any): Lancamento {
+        return Object.assign(new Lancamento, jsonData);
+    }
+
+    private handleError(error: any): Observable<any> {
+        console.log("ERRO NA REQUISIÇÃO =>", error);
+        return throwError(error);
+    }
+
+}
