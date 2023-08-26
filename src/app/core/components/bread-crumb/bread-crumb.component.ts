5621e4c7df643501568b5debf7e0934f3932e292
Valdeir Nascimento <valdeircn11@gmail.com>
2023-08-26 17:59:13 -0300

    Implementação de método para obter ultimo item do componente bread-crumb

--- src/app/core/components/bread-crumb/bread-crumb.component.ts
+++ src/app/core/components/bread-crumb/bread-crumb.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+interface BreadCrumbItem {
+    text: string;
+    link?: string
+}
 
 @Component({
-  selector: 'app-bread-crumb',
-  templateUrl: './bread-crumb.component.html',
-  styleUrls: ['./bread-crumb.component.css']
+    selector: 'app-bread-crumb',
+    templateUrl: './bread-crumb.component.html',
+    styleUrls: ['./bread-crumb.component.css']
 })
 export class BreadCrumbComponent implements OnInit {
 
-  constructor() { }
+    @Input() items: BreadCrumbItem[] = [];
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    obterUltimoItem(item: BreadCrumbItem): boolean {
+        const index = this.items.indexOf(item);
+        return index + 1 == this.items.length;
+    }
 
 }
